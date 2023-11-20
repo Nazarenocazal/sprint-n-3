b90b9929ff6613e5248b0434547046fc46993eaa
Nazareno <ezequielcazal372@gmail.com>
2023-11-20 17:14:30 -0300

    ever

---
+++ Js/script.js
@@ -0,0 +1,106 @@
+//Pruebas de console.log
+
+console.log('Bienvenidos a la consola Good coding.');
+
+//alert('Aceptar para continuar navegando')
+let respuesta = prompt("¿La pagina funciona de manera correcta?");
+console.log(respuesta);
+alert('Su respuesta fue: ' + respuesta)
+
+let confirmacion = confirm("¿Queres viajar?")
+if(confirmacion == true){
+    alert ("Genial bienvenido/a");
+}
+else{
+    alert("Es una lastima,adíos.")
+}
+
+
+//let edad=24;
+//console.log(edad);
+//console.log('tengo ' + edad + ' años');
+
+//edad=25;
+//console.log('ahora tengo ' + edad + ' años');
+
+//let resulado=10+10*2;
+//console.log('la respuesta es ' + resulado);
+
+//let cuenta1=10;
+//let cuenta2=10;
+//let cuenta3=3;
+//let resultado= cuenta1 + cuenta2 * cuenta3;
+//console.log('la respuesta es ' + resultado)
+
+//let misAmigos=['daniel','gabriel','alexis']
+//console.log(misAmigos)
+
+//let alumno1={
+   //nombre: 'nazareno',
+   // edad: 24,
+   //aprobado: true,
+//};
+
+//let alumno2={
+    //nombre: 'agustin',
+    //edad:20,
+    //aprobado:false,
+//};
+//console.log(alumno1)
+//console.log(alumno2)
+//console.log(alumno1.nombre);
+//alumno1.edad=25
+//console.log(alumno1);
+//alumno1.genero='masculino'
+//console.log(alumno1)
+
+
+//let  numero=6
+//if(numero==5){
+    //console.log('es correcto es 5');
+//} else {
+    //console.log('es incorrecto no es 5')}
+
+    //let numero=-2;
+    //if(numero > 0){
+     //   console.log('el numero es positivo');
+   // } else if (numero ===0){
+     //   console.log('el numnero es zero');
+    //} else{
+       // console.log('el numero es negativo');}
+
+//let contador=0
+//while(contador < 10){
+   // console.log('hola');
+    //contador=contador +2;}
+    
+//for(let contador=0;contador<10;contador=contador+2){
+  //  console.log('contador');}
+
+  //function saludar(nombre, edad){
+    //console.log('aloha '+ nombre);
+    //console.log('tu edad es '+ edad);}
+
+  //saludar('nazareno', 24)
+
+  
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
